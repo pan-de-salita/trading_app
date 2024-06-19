f6c679c9530ad3c3ef6fbf83bd523f326c6a5068
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-06-19 19:19:57 +0800

    docs: add documentation for viewing Status.status_type and User.role

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,7 +1,8 @@
 class Status < ApplicationRecord
   belongs_to :user
 
-  enum :status_types, {
+  # to view all of the status_types, use Status.status_type.keys
+  enum :status_type, {
     pending: 'pending',
     approved: 'approved',
     denied: 'denied'
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_one :status, dependent: :destroy
   accepts_nested_attributes_for :status
 
+  # to view all roles, use User.role.keys
   enum :role, {
     admin: 'admin',
     trader: 'trader'
