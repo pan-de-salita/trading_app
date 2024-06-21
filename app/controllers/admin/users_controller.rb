9b1c7db1f5f5c5325bff89bb810fced54def5b47
Lindsay Joseph Martinez <mlindsayjoseph@gmail.com>
2024-06-21 20:01:01 +0800

    fix: users can now access their accounts for edit
    fix: admins can no longer see their own account in all users column [security fix]

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,6 @@
 class Admin::UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :authorize_admin
+  before_action :authorize_user, only: %i[edit update destroy create]
   before_action :set_user, only: %i[edit update destroy]
 
   def index
@@ -20,11 +20,23 @@ class Admin::UsersController < ApplicationController
     end
   end
 
-  def edit; end
+  def edit
+    console
+  end
 
   def update
+    if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
+      params[:user].delete(:password)
+      params[:user].delete(:password_confirmation)
+    end
+
     if @user.update(user_params)
-      redirect_to admin_users_path, notice: 'User successfully updated!'
+      flash[:success] = 'Edit Successful.'
+      if current_user.admin?
+        redirect_to admin_users_path
+      else
+        redirect_to edit_user_registration_path
+      end
     else
       render :edit
     end
@@ -37,8 +49,11 @@ class Admin::UsersController < ApplicationController
 
   private
 
-  def authorize_admin
-    redirect_to root_path, alert: 'Only administrators can access this page.' unless current_user.admin?
+  # Make sure user cannot access admin privileges
+  def authorize_user
+    return if current_user.admin? || current_user == @user
+
+    redirect_to root_path, alert: 'You are not authorized to access this page.'
   end
 
   def set_user
