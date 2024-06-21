edb316eec49e4c7e2604050f910a221731007cd2
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-06-22 00:57:51 +0800

    chore: remove confirmable from users

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,16 @@
 Rails.application.routes.draw do
-
   devise_for :users, controllers: {
-    registrations: "users/registrations"
+    registrations: 'users/registrations'
   }
 
   namespace :admin do
-    resources :users, only: [:new, :create, :edit, :index, :update, :destroy]
+    resources :users, only: %i[new create edit index update destroy]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
   # Defines the root path route ("/")
-  root to: "pages#home"
-    
+  root to: 'pages#home'
 end
