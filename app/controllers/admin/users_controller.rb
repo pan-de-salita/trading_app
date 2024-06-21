fa6f97d197e0d40036118d7631a5921943c10087
Lindsay Joseph Martinez <mlindsayjoseph@gmail.com>
2024-06-21 20:01:01 +0800

    fix: users can now access their accounts for edit
    fix: admins can no longer see their own account in all users column [security fix]

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,54 +1,69 @@
-class Admin::UsersController < ApplicationController
-    before_action :authenticate_user!
-    before_action :authorize_admin
-    before_action :set_user, only: [:edit, :update, :destroy]
+    class Admin::UsersController < ApplicationController
+        before_action :authenticate_user!
+        before_action :authorize_user, only: [:edit, :update, :destroy, :create]
+        before_action :set_user, only: [:edit, :update, :destroy]
 
-    def index
-        @user = User.all
-    end
-
-    def new
-        @user = User.new
-    end
-
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            redirect_to root_path, notice: "User created successfully."
-        else
-            render :new
-        end
-    end
-
-    def edit
-    end
-
-    def update
-        if @user.update(user_params)
-            redirect_to admin_users_path, notice: "User successfully updated!"
-        else
-            render :edit
-        end
-    end
-
-    def destroy
-        @user.destroy
-        redirect_to admin_users_path, notice: "User deleted successfully."
-      end
-
-    private
-        def authorize_admin
-            redirect_to root_path, alert: "Only administrators can access this page." unless current_user.admin?
+        def index
+            @user = User.all
         end
 
-        def set_user
-            @user = User.find(params[:id])
+        def new
+            @user = User.new
         end
 
-        def user_params
-            params.require(:user).permit(:email, :password, :password_confirmation, :role)
+        def create
+            @user = User.new(user_params)
+            if @user.save
+                redirect_to root_path, notice: "User created successfully."
+            else
+                render :new
+            end
         end
 
-       
+        def edit
+            console
+        end
 
-end
+        def update
+            if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
+              params[:user].delete(:password)
+              params[:user].delete(:password_confirmation)
+            end
+          
+            if @user.update(user_params)
+              flash[:success] = "Edit Successful."
+              if current_user.admin?
+                redirect_to admin_users_path
+              else
+                redirect_to edit_user_registration_path
+              end
+            else
+              render :edit
+            end
+          end
+    
+
+        def destroy
+            @user.destroy
+            redirect_to admin_users_path, notice: "User deleted successfully."
+        end
+
+        private
+        # Make sure user cannot access admin privileges
+            def authorize_user
+                unless current_user.admin? || current_user == @user
+                    redirect_to root_path, alert: "You are not authorized to access this page."
+                end
+            end
+
+            def set_user
+                @user = User.find(params[:id])
+            end
+
+            def user_params
+                params.require(:user).permit(:email, :password, :password_confirmation, :role)
+            end
+
+        
+
+    end
