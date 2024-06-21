4691b76fe3e94e8b3139f9183e5332eefa7b4dcb
redroboto <vince.pleyto@gmail.com>
2024-06-21 19:53:50 +0800

    new: create Stock model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_240_622_020_820) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_21_114920) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -19,12 +19,19 @@ ActiveRecord::Schema[7.1].define(version: 20_240_622_020_820) do
   create_enum 'role', %w[admin trader]
   create_enum 'status_type', %w[pending approved denied]
 
-  create_table 'statuses', force: :cascade do |t|
-    t.enum 'status_type', default: 'pending', null: false, enum_type: 'status_type'
-    t.bigint 'user_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_statuses_on_user_id'
+  create_table "statuses", force: :cascade do |t|
+    t.enum "status_type", default: "pending", null: false, enum_type: "status_type"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_statuses_on_user_id"
+  end
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "ticker"
+    t.string "company_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table 'users', force: :cascade do |t|
