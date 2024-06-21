87704ec6ab1d7d30340496c063d8c4dea785ff75
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-06-21 19:36:28 +0800

    chore: add debug text to clarify params and path

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-   before_action :configure_sign_up_params, only: [:create]
-   before_action :configure_account_update_params, only: [:update]
-  #  before_action :check_admin, only: [:new, :create, :edit, :update, :destroy]
-   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
+  before_action :check_admin, only: %i[new create edit update destroy]
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   # GET /resource/sign_up
   # def new
@@ -43,14 +43,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-   def configure_sign_up_params
-     devise_parameter_sanitizer.permit(:sign_up, keys: [:status, :role])
-   end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[status role])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
-   def configure_account_update_params
-     devise_parameter_sanitizer.permit(:account_update, keys: [:status, :role])
-   end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[status role])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
@@ -67,15 +67,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   private
 
-    # def check_admin
-    #   if current_user.present? && !current_user.admin?
-    #     redirect_to root_path, notice: "Only administrators can view this page."
-    #   end
-    # end
+  def check_admin
+    return unless current_user.present? && !current_user.admin?
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :role])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation, :current_password, :role])
-    end
+    redirect_to root_path, alert: 'Only administrators can view this page.'
+  end
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email password password_confirmation role])
+    devise_parameter_sanitizer.permit(:account_update,
+                                      keys: %i[email password password_confirmation current_password role])
+  end
 end
