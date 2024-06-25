4769aa52ddd9e38a8316b02ca33fb726fe5c1a9d
redroboto <vince.pleyto@gmail.com>
2024-06-25 14:34:21 +0800

    feat: add api call to get stock timeseries

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,4 +2,17 @@ class Stock < ApplicationRecord
   has_many :transactions, dependent: :destroy
   has_many :users, through: :transactions
 
+  before_validate :fetch_stock_info
+
+  def fetch_stock_info
+    # Api thing
+    stock_timeseries = Alphavantage::TimeSeries.search(keywords: ticker.to_s)
+    stock_info = stock_timeseries.daily(outputsize: 'compact')
+
+    update(
+      company_name: BaseStocks.find(ticker: ticker.to_s).company_name,
+      # most updated close price
+      share_price: stock_info.time_series_daily.first[1].close
+    )
+  end
 end
