db5f3179980b4678d47fed528e3ea923b515a46d
redroboto <vince.pleyto@gmail.com>
2024-06-26 20:12:02 +0800

    feat: add search bar in stocks page using Ransack

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,14 @@
 class Stock < ApplicationRecord
   has_many :transactions, dependent: :destroy
   has_many :users, through: :transactions
+
+  def self.ransackable_attributes(auth_object = nil)
+    authorizable_ransackable_attributes
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    authorizable_ransackable_associations
+  end
+
 end
 
