e0454646c1cd0c3a58e83f347f356d4c68a86ce8
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-06-26 19:41:48 +0800

    feat: readd stock controllers

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -9,22 +9,19 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  def create
-    super
-    return root_path if resource.is_a?(User)
-
-    new_user_session_path
-
-    # NOTE: Is .active_for_authentication? called via super?
-    # According to Devise's cource code (https://github.com/heartcombo/devise/blob/a259ff3c28912a27329727f4a3c2623d3f5cb6f2/lib/devise/models/authenticatable.rb), it might be. See:
-    #
-    # (.valid_for_authentication?) Check if the current object is valid for authentication.
-    # This method and find_for_authentication are the methods used in a Warden::Strategy
-    # to check if a model should be signed in or not.
-    #
-    # However, you should not overwrite this method, you should overwrite active_for_authentication?
-    # and inactive_message instead.
-  end
+  # def create
+  #   super
+  #
+  # NOTE: Is .active_for_authentication? called via super?
+  # According to Devise's cource code (https://github.com/heartcombo/devise/blob/a259ff3c28912a27329727f4a3c2623d3f5cb6f2/lib/devise/models/authenticatable.rb), it might be. See:
+  #
+  # (.valid_for_authentication?) Check if the current object is valid for authentication.
+  # This method and find_for_authentication are the methods used in a Warden::Strategy
+  # to check if a model should be signed in or not.
+  #
+  # However, you should not overwrite this method, you should overwrite active_for_authentication?
+  # and inactive_message instead.
+  # end
 
   # DELETE /resource/sign_out
   # def destroy
