8a48d9a4d432566237cdac9a2fc5f83f929dc853
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-06-26 22:59:39 +0800

    init commit of branch fetch_news

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,11 +1,10 @@
 class StocksController < ApplicationController
   def index
-    
     @q = Stock.ransack(params[:q])
     @stocks = @q.result(distinct: true)
 
     # For testing only:
-    #@stocks = Stock.first(5)
+    # @stocks = Stock.first(5)
 
     # TODO: incorporate ransack
     # NOTE: ransack configs are done on the model level, i believe
