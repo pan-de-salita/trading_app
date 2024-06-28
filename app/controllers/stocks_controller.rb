dd1df97813487a1ea2643ed776c57745fc89418a
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-06-28 16:25:52 +0800

    potential_fix: add workaround for alphavantage api call limit

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -4,49 +4,23 @@ class StocksController < ApplicationController
   def index
     @q = Stock.ransack(params[:q])
     @stocks = params[:q] ? @q.result(distinct: true) : []
-
-    # NOTE: For debugging only
-    return unless params[:q]
-
-    @search_item = params[:q][:company_name_or_ticker_cont]
   end
 
   def show
     @stock = Stock.find(params[:id])
-    p @stock
-    p @stock.ticker
 
-    if @stock.data.nil? || @stock.updated_at.utc.strftime('%Y-%m-%d') != DateTime.now.strftime('%Y-%m-%d')
-      stock_timeseries = Alphavantage::TimeSeries.new(symbol: @stock.ticker).daily(outputsize: 'compact')
-      @stock.update(data: stock_timeseries.to_json) unless stock_timeseries.information
-
-      p 'STOCK'
-      p @stock
-      p 'STOCK'
-    end
-
-    if @stock.news.nil? || @stock.updated_at.utc.strftime('%Y-%m-%d') != DateTime.now.strftime('%Y-%m-%d')
-      stock_news = Alphavantage::Client.new(function: 'NEWS_SENTIMENT', tickers: @stock.ticker).json
-      @stock.update(news: stock_news.to_json) unless stock_news.information
-
-      p 'NEWS'
-      p @stock
-      p 'NEWS'
-    end
+    @stock.set_or_fetch_stock_data
+    @stock.set_or_fetch_stock_news
 
     @stock_timeseries = JSON.parse(@stock.data) unless @stock.data.nil?
     @stock_news = JSON.parse(@stock.news) unless @stock.news.nil?
-
-    p 'OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO'
-    p @stock_timeseries
-    p @stock_news
-    p 'OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO'
   end
 
   private
 
   def set_general_news
     general_news = Alphavantage::Client.new(function: 'NEWS_SENTIMENT').json
-    @general_news = general_news.information ? general_news.feed : nil
+    session[:general_news] = general_news['feed'].first(9) unless general_news.information
+    @general_news = !general_news.information ? JSON.parse(general_news)['feed'].first(9) : session[:general_news]
   end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,13 +2,33 @@ class Stock < ApplicationRecord
   has_many :transactions, dependent: :destroy
   has_many :users, through: :transactions
 
-  def self.ransackable_attributes(auth_object = nil)
+  def self.ransackable_attributes(_auth_object = nil)
     authorizable_ransackable_attributes
   end
 
-  def self.ransackable_associations(auth_object = nil)
+  def self.ransackable_associations(_auth_object = nil)
     authorizable_ransackable_associations
   end
 
-end
+  def set_or_fetch_stock_data
+    return unless data.nil? || updated_at.utc.strftime('%Y-%m-%d') != DateTime.now.strftime('%Y-%m-%d')
 
+    fetch_stock_information(:data)
+  end
+
+  def set_or_fetch_stock_news
+    return unless news.nil? || updated_at.utc.strftime('%Y-%m-%d') != DateTime.now.strftime('%Y-%m-%d')
+
+    fetch_stock_information(:news)
+  end
+
+  def fetch_stock_information(attr)
+    if attr == :data
+      response = Alphavantage::TimeSeries.new(symbol: ticker).daily(outputsize: 'compact')
+    elsif attr == :news
+      response = Alphavantage::Client.new(function: 'NEWS_SENTIMENT', tickers: ticker).json
+    end
+
+    update(attr: (attr == :data ? response.to_json : response.to_json.feed)) unless response['information']
+  end
+end
