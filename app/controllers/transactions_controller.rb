ab00c507381ce46e51f43c09951aed8ac8278f7c
redroboto <vince.pleyto@gmail.com>
2024-06-29 10:49:41 +0800

    feat: add save new transaction

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,12 +1,30 @@
 class TransactionsController < ApplicationController
-  
+
   def index
+    @transactions = current_user.transactions
   end
 
-  def new
+  # def new
+
+  # end
+
+  def create
+    stock_price = Stock.find(transaction_params[:stock_id].to_i).price
+    @transaction = current_user
+    .transactions.build(transaction_params
+    .merge({share_price: stock_price, amount: transaction_params[:share_qty].to_i * stock_price }))
+    #added merge to as workaround for disabled fields
+
+    if @transaction.save
+      redirect_to stock_path(@transaction.stock_id), notice: "Stock successfully bought"
+    end
   end
 
-  def create 
+
+  private
+
+  def transaction_params
+    params.require(:transaction).permit(:amount, :share_price, :share_qty, :transaction_type, :stock_id)
   end
 
 end
