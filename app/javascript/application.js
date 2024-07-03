c18d7cf9f26a731453db66060ea0ae39c5614e5f
Lindsay Joseph Martinez <mlindsayjoseph@gmail.com>
2024-07-04 01:39:35 +0800

    feat: add charting logic

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,8 +2,12 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require echarts.min.js
+//= require echarts/theme/dark.js
+
 
 import "@hotwired/turbo-rails";
 import "controllers";
 import 'bootstrap';
 import '@popperjs/core';
+
