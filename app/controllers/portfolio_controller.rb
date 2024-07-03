22326ee91b4ec6463931feb3397867f4f4f24e1a
redroboto <vince.pleyto@gmail.com>
2024-07-03 19:03:00 +0800

    feat: remove stocks with zero shares from portfolio view

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -3,12 +3,14 @@ class PortfolioController < ApplicationController
 
   def index
     @stocks = current_user.stocks
+                          .select {|s| s.transactions.total_shares > 0 }
     @transactions = current_user.transactions
     console
   end
 
   def show
     @stocks = current_user.stocks
+                          .select {|s| s.transactions.total_shares > 0 }
     @transactions = current_user.transactions
     @stock = Stock.find(params[:id])
   end
