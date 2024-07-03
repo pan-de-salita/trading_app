ef40e2fa20f4b140e28f86395380adccfc98faca
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-07-03 19:34:12 +0800

    refactor: select only stocks with positive shares in portfolio

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -3,22 +3,20 @@ class PortfolioController < ApplicationController
   before_action :update_current_user_stocks
 
   def index
-    @stocks = current_user.stocks
-                          .select {|s| s.transactions.total_shares > 0 }
+    @stocks = current_user.stocks.with_positive_total_shares
     @transactions = current_user.transactions
     console
   end
-  
+
   def show
-    @stocks = current_user.stocks
-                          .select {|s| s.transactions.total_shares > 0 }
+    @stocks = current_user.stocks.with_positive_total_shares
     @transactions = current_user.transactions
     @stock = Stock.find(params[:id])
   end
-  
+
   private
 
   def update_current_user_stocks
-    current_user.stocks.each(&:set_or_fetch_stock_data)
+    current_user.stocks.with_positive_total_shares.each(&:set_or_fetch_stock_data)
   end
 end
