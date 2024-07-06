986cfea05f8b185264434dae0b39561271dd01c9
Lindsay Joseph Martinez <mlindsayjoseph@gmail.com>
2024-07-07 01:57:45 +0800

    refactor: transaction history on admin end
    feat: add sort function

--- app/controllers/admin/transactions_controller.rb
+++ app/controllers/admin/transactions_controller.rb
@@ -4,7 +4,21 @@ class Admin::TransactionsController < ApplicationController
 
   def index
     @transactions = Transaction.all
+    
+    case params[:sort_by]
+    when 'created_at'
+      @transactions = @transactions.order(created_at: :desc)
+    when 'created_at_asc'
+      @transactions = @transactions.order(created_at: :asc)
+    when 'company_name'
+      @transactions = @transactions.joins(:stock).order('stocks.company_name')
+    when 'transaction_type'
+      @transactions = @transactions.order(transaction_type: :asc)
+    else
+      @transactions = @transactions.order(created_at: :desc)
+    end
   end
+
 
 private
 
