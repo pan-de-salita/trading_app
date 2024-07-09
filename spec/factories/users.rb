e8e12f92496238a3bb01bdc2fe1ca4b60184488f
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-07-10 00:27:52 +0800

    refactor: add traits for user factory and make available FactoryBot methods

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,24 @@
 FactoryBot.define do
   factory :user do
-    sequence(:email) { |n| "test_#{n}@mail.com" }
     password { 'foobar' }
     password_confirmation { 'foobar' }
+
+    trait :trader do
+      sequence(:email) { |n| "trader_#{n}@mail.com" }
+      role { 'trader' }
+    end
+
+    trait :admin do
+      email { 'admin@test.com' }
+      role { 'admin' }
+    end
+
+    trait :confirmed_email do
+      confirmed_at { DateTime.now }
+    end
+
+    trait :admin_created do
+      admin_created { true }
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,34 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let!(:trader) { FactoryBot.create(:user) }
+  let!(:trader) { create(:user, :trader) }
+  let!(:admin) { create(:user, :admin) }
 
   it 'should have a unique email address' do
-    trader_using_existing_email = FactoryBot.build(:user, email: trader.email)
+    trader_using_existing_email = build(:user, email: trader.email)
     expect(trader_using_existing_email).to_not be_valid
     expect(trader_using_existing_email.errors.full_messages).to include('Email has already been taken')
   end
 
   it 'should having matching password and password_confirmation' do
-    trader_with_mismatching_passwords = FactoryBot.build(
-      :user,
-      password: 'foobar',
-      password_confirmation: 'barfoo'
-    )
+    trader_with_mismatching_passwords = build(:user, password: 'foobar', password_confirmation: 'barfoo')
     expect(trader_with_mismatching_passwords).to_not be_valid
-    expect(trader_with_mismatching_passwords.errors.full_messages)
-      .to include("Password confirmation doesn't match Password")
+    expect(trader_with_mismatching_passwords.errors.full_messages).to include("Password confirmation doesn't match Password")
   end
 
   it 'should only acceptable roles' do
     acceptable_roles = %w[admin trader]
-    acceptable_roles.each { |accepted_role| expect(FactoryBot.build(:user, role: accepted_role)).to be_valid }
+    acceptable_roles.each_with_index do |accepted_role, idx|
+      expect(build(:user, email: "#{accepted_role}_#{idx}@mail.com", role: accepted_role)).to be_valid
+    end
   end
 
   it 'should not carry unacceptable roles' do
     unacceptable_roles = %w[frontend_dev backend_dev fullstack_dev]
-    unacceptable_roles.each do |unacceptable_role|
-      user = User.new(role: unacceptable_role)
+    unacceptable_roles.each_with_index do |unacceptable_role, idx|
+      user = build(:user, email: "#{accepted_role}_#{idx}@mail.com", role: unacceptable_role)
       expect(user).to raise_error(ArgumentError)
     rescue StandardError => e
       p "Caught error: #{e.message}"
@@ -36,8 +34,7 @@ RSpec.describe User, type: :model do
   end
 
   it "should be of role 'trader' if no role was specified upon creation" do
-    trader_with_no_role_specified = User.new(email: 'test@mail.com', password: 'foobar',
-                                             password_confirmation: 'foobar', admin_created: false)
+    trader_with_no_role_specified = build(:user, :trader)
     expect(trader_with_no_role_specified).to be_valid
     expect(trader_with_no_role_specified.role).to eq('trader')
   end
@@ -52,13 +49,12 @@ RSpec.describe User, type: :model do
   end
 
   it "should have 'approved' status if admin_created" do
-    trader_created_by_admin = FactoryBot.create(:user, admin_created: true)
+    trader_created_by_admin = create(:user, :trader, :admin_created)
     expect(trader_created_by_admin.status.approved?).to eq(true)
   end
 
   it "should automatically have 'approved' status if created as an admin" do
-    admin_user = FactoryBot.create(:user, role: 'admin')
-    expect(admin_user.status.approved?).to eq(true)
+    expect(admin.status.approved?).to eq(true)
   end
 
   it 'should not be active for authentication if not admin or not confirmed' do
@@ -68,16 +64,15 @@ RSpec.describe User, type: :model do
   end
 
   it 'should be active for authentication if carries a confirmed_at attribute' do
-    trader.update(confirmed_at: DateTime.now)
-    expect(trader.admin?).to eq(false)
-    expect(trader.confirmed_at?).to eq(true)
-    expect(trader.active_for_authentication?).to eq(true)
+    confirmed_trader = create(:user, :trader, :confirmed_email)
+    expect(confirmed_trader.admin?).to eq(false)
+    expect(confirmed_trader.confirmed_at?).to eq(true)
+    expect(confirmed_trader.active_for_authentication?).to eq(true)
   end
 
   it 'should be active for authentication if an admin' do
-    trader.update(role: 'admin')
-    expect(trader.admin?).to eq(true)
-    expect(trader.confirmed_at?).to eq(false)
-    expect(trader.active_for_authentication?).to eq(true)
+    expect(admin.admin?).to eq(true)
+    expect(admin.confirmed_at?).to eq(false)
+    expect(admin.active_for_authentication?).to eq(true)
   end
 end
