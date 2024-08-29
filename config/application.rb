643ea6735593c1a10b304559e150267f224d4a99
redroboto <vince.pleyto@gmail.com>
2024-08-29 21:57:08 +0800

    fix: updated config/application.rb with cmd to enable use of asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module TradingApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    #configure Rails to use asset pipeline
+    config.assets.enabled = true
   end
 end
